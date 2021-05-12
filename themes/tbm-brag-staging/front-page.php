b322775f835742577f011c1ffa40e9347464e880
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-05-12 20:25:53 +1000

    Refactor

--- themes/tbm-brag-staging/front-page.php
+++ themes/tbm-brag-staging/front-page.php
@@ -5,5 +5,11 @@
 get_header();
 ?>
 
+<div class="ad-billboard container py-2">
+    <?php render_ad_tag('leaderboard'); 
+    ?>
+    <a href="https://thebrag.media?970x250" target="_blank"><img src="http://placehold.it/970x250/663366/fff"></a>
+</div>
+
 <?php
 get_footer();
