e103df26da7040b51865cf4acecb010a8c036f4e
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-05-25 09:50:42 +1000

    Rename theme

--- themes/tbm-brag-staging/page-templates/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
- * Template Name: Logout
- */
-
-wp_logout();
--- themes/tbm-brag-staging/template-parts/account/header.php
+++
@@ -1,5 +0,0 @@
-<?php if (is_user_logged_in()) : ?>
-    <div class="d-flex border-bottom pb-2">
-        <h1>My Dashboard</h1>
-    </div>
-<?php endif; ?>
