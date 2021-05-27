9bf3001d7b6dda86b0dbb74c94f476838a36818e
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-05-28 09:42:55 +1000

    Update footer.php

--- themes/tbm-brag/footer.php
+++ themes/tbm-brag/footer.php
@@ -13,7 +13,7 @@
   <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:200,500|Roboto:400,700&display=swap" rel="stylesheet"> -->
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400&family=Poppins:wght@200;300;500&display=swap" rel="stylesheet">
   <?php if (!is_page_template('page-quiz.php')) : ?>
-    <link rel="stylesheet" id="site-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" />
+    <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" />
   <?php endif; ?>
 </noscript>
 
