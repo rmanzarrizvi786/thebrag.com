4bf23b97ffaa0f4221dab34c7d206231d4da8534
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-05-27 10:58:21 +1000

    Update tile.php

--- themes/tbm-brag/template-parts/single/tile.php
+++ themes/tbm-brag/template-parts/single/tile.php
@@ -16,12 +16,12 @@
             </div>
             <div class="pl-2 post-content align-self-start col-7">
                 <?php if (isset($category)) : ?>
-                    <div class="mb-2 text-uppercase cat d-none d-md-block">
+                    <div class="mb-2 text-uppercase cat d-block d-md-none">
                         <?php echo $category; ?>
                     </div>
                 <?php endif; ?>
                 <h3 class="my-2"><?php the_title(); ?></h3>
-                <p class="excerpt d-block d-md-none">
+                <p class="excerpt d-none d-md-block">
                     <?php
                     $author_name =
                         get_field('photographer') ? get_field('photographer') : (get_field('author') ? get_field('author') : (get_field('Author') ? get_field('Author') : get_the_author_meta('first_name', $post->post_author) . ' ' . get_the_author_meta('last_name', $post->post_author)));
