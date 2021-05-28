b5fa031fdd9417da9fa4c751b7e6f2d2ea452ae3
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-05-28 10:49:23 +1000

    Fix top search position

--- themes/tbm-brag/header.php
+++ themes/tbm-brag/header.php
@@ -301,7 +301,7 @@ $page_template = get_page_template_slug();
     ?>
 
     <div class="nav-wrap container d-md-flex flex-column flex-md-row p-r">
-      <div id="top-search-wrap" style="position: absolute; top: -.25rem; left: -100%; z-index: 10; background: #fff; border-radius: .5rem; width: 320px; transition: .25s left linear; box-shadow: 15px 0 10px #000">
+      <div id="top-search-wrap" style="position: absolute; top: -.25rem; left: 0; z-index: 10; background: #fff; border-radius: .5rem; width: 320px; transition: .25s left linear; box-shadow: 15px 0 10px #000; display: none;">
         <form role="search" method="get" id="searchform" class="searchform d-flex justify-content-center" action="<?php echo esc_url(home_url('/')); ?>" style="">
           <div class="btn btn-close-top-search" style="padding: .25rem">
             <img src="<?php echo ICONS_URL; ?>chevron-thin-left-dark.svg" width="16" height="16" alt="<">
