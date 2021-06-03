130bc3cf4e1f853029b6f021ad0b9ad81be3c433
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-03 14:00:49 +1000

    Update scripts.js

--- themes/tbm-brag/js/scripts.js
+++ themes/tbm-brag/js/scripts.js
@@ -264,8 +264,10 @@ jQuery(document).ready(function ($) {
                 ]);
 
                 var v = fusetag.getAdSlotsById('22339226185')[0].getResponseInformation(); // 22339226185 = Fuse ID for Billboard
-                if (typeof v.lineItemId != "undefined" && v.lineItemId == 5709731975) { // 5709731975 = Target Line Item ID in GAM (DFP)
-                  fusetag.setTargeting("pos", ["1"]);
+                if (typeof v != "undefined") {
+                  if (typeof v.lineItemId != "undefined" && v.lineItemId == 5709731975) { // 5709731975 = Target Line Item ID in GAM (DFP)
+                    fusetag.setTargeting("pos", ["1"]);
+                  }
                 } else {
                   fusetag.setTargeting("pagepath", [
                     "'" + res.data.pagepath + "'",
