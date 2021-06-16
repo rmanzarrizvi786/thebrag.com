5e937e7e7453323f6599caba0d605b6df9ed75a5
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-16 10:36:36 +1000

    Update menu on Observer

--- themes/tbm-brag/page-templates/brag-observer/menu.php
+++ themes/tbm-brag/page-templates/brag-observer/menu.php
@@ -1,13 +1,127 @@
 <?php
-$logo_link = isset( $logo_link ) ? $logo_link : site_url(); // ! isset( $logo_link ) ? site_url() : $logo_link;
-$logo_alt = isset( $logo_alt ) ? $logo_alt : 'The Brag';
+$top_menu_html = '';
+$number_of_menu_items = 8;
+$top_menu_items = [];
+$my_sub_lists = [];
+$exclude_cats = [288366, 303097, 288238, 284732]; // Competitions, Evergreen, News, Features
+
+if (is_user_logged_in()) :
+    $current_user = wp_get_current_user();
+    $user_id = $current_user->ID;
+    $my_subs = $wpdb->get_results("SELECT s.list_id FROM {$wpdb->prefix}observer_subs s JOIN {$wpdb->prefix}observer_lists l ON l.id = s.list_id WHERE user_id = '{$user_id}' AND s.status = 'subscribed' AND l.related_site='thebrag.com' ");
+    $my_sub_lists = wp_list_pluck($my_subs, 'list_id');
+endif;
+
+ob_start();
+
+if (isset($my_sub_lists) && !empty($my_sub_lists)) :
+    $menu_cats = get_categories(
+        array(
+            // 'parent' => null,
+            // 'hide_empty' => '0',
+            'orderby'    => 'count',
+            'order' => 'DESC',
+            'exclude' => $exclude_cats,
+            'meta_query' => array(
+                array(
+                    'key'     => 'observer-topic',
+                    'value'   => $my_sub_lists,
+                    'compare' => 'IN',
+                )
+            )
+        )
+    );
+    $menu_cats_ids = wp_list_pluck($menu_cats, 'term_id');
+
+    foreach ($menu_cats as $cat) :
+        array_push($top_menu_items, [
+            'link' => get_category_link($cat),
+            'text' => $cat->name,
+        ]);
+    endforeach;
+
+    if (count($menu_cats) < $number_of_menu_items) :
+        $menu_cats2 = get_categories(
+            array(
+                'parent' => null,
+                'orderby'    => 'count',
+                'order' => 'DESC',
+                'exclude' => array_merge($exclude_cats, $menu_cats_ids),
+                'number' => $number_of_menu_items - count($menu_cats)
+            )
+        );
+
+        // echo '<pre>'; print_r($menu_cats2);exit;
+        foreach ($menu_cats2 as $cat) :
+            array_push($top_menu_items, [
+                'link' => get_category_link($cat),
+                'text' => '<span class="plus"><img src="' . ICONS_URL . 'plus.svg" width="16" height="16" alt="+"></span>
+        <span class="plus-hover"><img src="' . ICONS_URL . 'plus-color.svg" width="16" height="16" alt="+"></span>
+        <span class="text-muted">' . $cat->name . '</span>',
+                'class' => 'secondary',
+            ]);
+        endforeach;
+    endif;
+else : // Show all categories
+    $menu_cats = get_categories(
+        array(
+            'parent' => null,
+            'orderby'    => 'count',
+            'order' => 'DESC',
+            'exclude' => $exclude_cats,
+        )
+    );
+
+    // echo '<pre>'; print_r($menu_cats2);exit;
+    foreach ($menu_cats as $cat) :
+        array_push($top_menu_items, [
+            'link' => get_category_link($cat),
+            'text' => $cat->name,
+        ]);
+    endforeach;
+endif; // If user picked niche
+
+array_push($top_menu_items, [
+    'link' => home_url('/observer/competitions/'),
+    'text' => 'Competitions',
+]);
+?>
+<nav class="menu-top-menu-container">
+    <ul id="menu_main" class="nav flex-column">
+        <?php
+        foreach ($top_menu_items as $i => $top_menu_item) :
+        ?>
+            <li class="nav-item">
+                <a href="<?php echo $top_menu_item['link']; ?>" class="nav-link"><?php echo $top_menu_item['text']; ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
+<?php
+// var_dump($menu_cats); exit;
+/* else :
+    wp_nav_menu(array(
+      'theme_location' => 'top',
+      'menu_id'        => 'menu_main',
+      'menu_class' => 'nav flex-column flex-md-row',
+      'fallback_cb'   => false,
+      'add_li_class'  => 'nav-item',
+      'link_class'   => 'nav-link',
+      'container' => 'nav',
+    ));
+   */
+
+$top_menu_html = ob_get_clean();
+
+$logo_link = isset($logo_link) ? $logo_link : site_url(); // ! isset( $logo_link ) ? site_url() : $logo_link;
+$logo_alt = isset($logo_alt) ? $logo_alt : 'The Brag';
 ?>
 <div id="mobile-menu" class="d-flex flex-column">
-  <div style="position: absolute; top: 0; left: 0;">
-  <button class="navbar-toggler" type="button" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation" style="outline: none;">
-      <div class="navbar-button-bars text-white"><i class="fa fa-remove"></i></div>
-  </button>
-</div>
+    <div style="position: absolute; top: 0; left: 0;">
+        <button class="navbar-toggler" type="button" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation" style="outline: none;">
+            <div class="navbar-button-bars text-white"><i class="fa fa-remove"></i></div>
+        </button>
+    </div>
     <div class="brand my-3">
         <a class="header-logo" href="<?php echo $logo_link; ?>">
             <img src="<?php echo $logo_url; ?>" alt="<?php echo $logo_alt; ?>" class="img-fluid" style="max-width: 150px;" loading="lazy">
@@ -22,34 +136,11 @@ $logo_alt = isset( $logo_alt ) ? $logo_alt : 'The Brag';
             <li class="nav-item"><a href="<?php echo home_url('/observer/'); ?>" class="nav-link px-2 text-white"><i class="fa fa-envelope fa-lg" aria-hidden=true></i></a></li>
         </ul>
     </nav>
-    <form role="search" method="get" id="searchform-mobile" class="searchform form-inline justify-content-center my-1" action="<?php echo esc_url( home_url( '/' ) ); ?>" style="">
+    <form role="search" method="get" id="searchform-mobile" class="searchform form-inline justify-content-center my-1" action="<?php echo esc_url(home_url('/')); ?>" style="">
         <div><input type="text" name="s" class="search-field form-control" placeholder="Search..." autocomplete="off" aria-label="Search"></div>
         <button type="submit" class="btn" aria-label="Search"><i class="fa fa-search" aria-hidden="true"></i></button>
     </form>
-    <div id="menu" class="mt-3" style="width: 300px; margin: auto;">
-      <h4 class="mt-2 text-white menu-genres-header pb-2" style="margin-left: .7rem;">What do you love?</h4>
-        <?php
-        wp_nav_menu( array(
-            'theme_location' => 'top-what-you-love',
-            'menu_id'        => 'menu_main-love',
-            'menu_class' => 'nav flex-column',
-            'fallback_cb'   => false,
-            'add_li_class'  => 'nav-item',
-            'link_class'   => 'nav-link'
-            ) );
-        ?>
-    </div>
-    <div id="menu-genres" class="my-3" style="width: 300px; margin: auto;">
-      <h4 class="mt-2 text-white menu-genres-header pb-2" style="margin-left: .7rem;">Check this out</h4>
-        <?php
-        wp_nav_menu( array(
-            'theme_location' => 'top-check-this-out',
-            'menu_id'        => 'menu_main-checkout',
-            'menu_class' => 'nav flex-column',
-            'fallback_cb'   => false,
-            'add_li_class'  => 'nav-item',
-            'link_class'   => 'nav-link'
-            ) );
-        ?>
-    </div>
+    <?php
+    echo $top_menu_html;
+    ?>
 </div>
