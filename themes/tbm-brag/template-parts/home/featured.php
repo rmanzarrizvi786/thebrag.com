1b7c02a55e92c5ce1f1c6d584a5966e4d935276f
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-11-08 15:58:22 +1100

    Update featured.php

--- themes/tbm-brag/template-parts/home/featured.php
+++ themes/tbm-brag/template-parts/home/featured.php
@@ -62,7 +62,7 @@ $trending_story_ID = $trending_story->ID;
                                 <?php
                                 if (isset($author_img_src)) {
                                     if ($author_img_src) {
-                                        echo  '<img src="' . $author_img_src[0] . '" class="rounded" width="24" height="24" style="height: 100%; width: auto; max-width: none; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">';
+                                        echo  '<img src="' . $author_img_src . '" class="rounded" width="24" height="24" style="height: 100%; width: auto; max-width: none; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">';
                                     }
                                 } else {
                                     echo get_avatar($trending_story->post_author, 24, CDN_URL . 'default-avatar-4.png', '', array('class' => 'rounded'));
