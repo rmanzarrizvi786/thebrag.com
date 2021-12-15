25666241f3670546f111d3cb3fd9c9a8e334d9ad
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-12-15 18:07:53 +1100

    Update unsubscribe.php

--- themes/tbm-brag/page-templates/unsubscribe.php
+++ themes/tbm-brag/page-templates/unsubscribe.php
@@ -24,6 +24,9 @@ if (isset($_GET['type'])) {
 
     $unsub_type_e = explode('_', $unsub_type);
     $site_abbr = $unsub_type_e[0];
+
+    $unsub_attr = str_replace($site_abbr . '_', '', $unsub_type);
+
     if (!in_array($site_abbr, $site_abbrs)) {
       $valid_link = false;
     }
@@ -59,21 +62,21 @@ if ($valid_link && isset($_GET['token'])) {
         if (!is_null($braze_user['user']) && isset($braze_user['user']->custom_attributes)) {
           if (isset($braze_user['user']->custom_attributes->{$custom_attribute_unsub})) {
             if (is_array($braze_user['user']->custom_attributes->{$custom_attribute_unsub})) {
-              $unsubs = array_merge($braze_user['user']->custom_attributes->{$custom_attribute_unsub}, [$unsub_type]);
+              $unsubs = array_merge($braze_user['user']->custom_attributes->{$custom_attribute_unsub}, [$unsub_attr]);
 
               // var_dump($unsubs);
               // exit;
             } else {
               $unsubs = [
                 $braze_user['user']->custom_attributes->{$custom_attribute_unsub},
-                $unsub_type
+                $unsub_attr
               ];
             }
           } else {
-            $unsubs = [$unsub_type];
+            $unsubs = [$unsub_attr];
           }
         } else {
-          $unsubs = [$unsub_type];
+          $unsubs = [$unsub_attr];
         }
 
         $braze_payload = [];
@@ -105,37 +108,42 @@ if (!$valid_link) {
 get_header();
 ?>
 
-<div class="container">
+<div class="ad-billboard ad-billboard-top container py-1 py-md-2">
+  <div class="mx-auto text-center">
+    <?php render_ad_tag('leaderboard'); ?>
+  </div>
+</div>
+
+
+<div class="container bg-yellow">
   <div class="row justify-content-center">
-    <div id="update-profile" class="col-sm-9 col-lg-9 my-5">
-      <main class="site-main" role="main">
+    <div id="update-profile" class="col-sm-9 col-lg-9 my-3">
 
-        <?php if (!empty($errors)) : ?>
-          <div class="alert alert-danger">
-            <?php foreach ($errors as $error) : ?>
-              <div><?php echo $error; ?></div>
-            <?php endforeach; ?>
-          </div>
-        <?php endif; ?>
+      <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+          <?php foreach ($errors as $error) : ?>
+            <div><?php echo $error; ?></div>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>
 
-        <?php if (!empty($messages)) : ?>
-          <div class="alert alert-success">
-            <?php foreach ($messages as $message) : ?>
-              <div><?php echo $message; ?></div>
-            <?php endforeach; ?>
-          </div>
+      <?php if (!empty($messages)) : ?>
+        <div class="alert alert-success">
+          <?php foreach ($messages as $message) : ?>
+            <div><?php echo $message; ?></div>
+          <?php endforeach; ?>
+        </div>
 
-          <?php if (0 && !is_null($redirectTo)) : ?>
-            <script>
-              window.setTimeout(function() {
-                window.location = '<?php echo $redirectTo; ?>';
-              }, 3000);
-            </script>
-          <?php endif; // If redirecting 
-          ?>
-        <?php endif; ?>
+        <?php if (0 && !is_null($redirectTo)) : ?>
+          <script>
+            window.setTimeout(function() {
+              window.location = '<?php echo $redirectTo; ?>';
+            }, 3000);
+          </script>
+        <?php endif; // If redirecting 
+        ?>
+      <?php endif; ?>
 
-      </main>
     </div>
   </div>
 </div>
