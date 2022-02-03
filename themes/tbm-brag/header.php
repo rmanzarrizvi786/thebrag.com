f7c0ef6c3a947e1a9f127ac25c0fba71e63b62e7
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-02-03 11:03:22 +1100

    Update header.php

--- themes/tbm-brag/header.php
+++ themes/tbm-brag/header.php
@@ -52,19 +52,12 @@ $current_url = home_url(add_query_arg([], $GLOBALS['wp']->request));
       $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
       if (isset($src[0])) {
   ?>
-        <meta property="og:image" content="<?php echo $src[0]; ?>" />
         <meta name="twitter:image" content="<?php echo $src[0]; ?>">
         <link rel="preconnect" href="<?php echo $src[0]; ?>">
     <?php
       } // If full featured image src is set
     } // If post featured image is set
     ?>
-    <meta property="og:title" content="<?php the_title(); ?>" />
-
-    <meta property="og:type" content="article" />
-    <meta property="og:site_name" content="The Brag" />
-    <meta property="og:url" content="<?php echo get_permalink(); ?>" />
-
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@TheBrag">
     <meta name="twitter:title" content="<?php the_title(); ?>">
