d6f6d54cf6d670e97afdd48893b567cc197f27aa
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-04-08 14:52:12 +1000

    Update preview-newsletter.php

--- themes/tbm-brag/page-templates/preview-newsletter.php
+++ themes/tbm-brag/page-templates/preview-newsletter.php
@@ -3,6 +3,11 @@
  * Template Name: Preview Newsletter
  */
 
+if (!isset($_GET['id'])) {
+    wp_redirect(home_url());
+    die();
+}
+
 $frontend = true;
 
 include WP_PLUGIN_DIR . '/brag-observer/partials/newsletter-template-braze.php';
