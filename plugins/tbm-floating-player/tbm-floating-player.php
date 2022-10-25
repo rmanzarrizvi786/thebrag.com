74d2c797ed81adec51566557b05d1882ba645980
Toby Smith <tobypsmith@gmail.com>
2022-10-25 11:14:39 +1100

    Revert "fix: video of week broken image"
    This reverts commit b69bc9e53c0156089c8e0e7e89b319784f5e6558.

--- plugins/tbm-floating-player/tbm-floating-player.php
+++ plugins/tbm-floating-player/tbm-floating-player.php
@@ -11,36 +11,35 @@
 
 namespace TBM;
 
-class FloatingPlayer {
-    protected $playerId;
-    protected $playlistId;
-    protected $playerTitle;
+class FloatingPlayer
+{
+  protected $playerId;
+  protected $playlistId;
+  protected $playerTitle;
 
-    public function __construct() {
-        $this->playerId = get_option('tbm_floating_dm_player_id', '');
-        $this->playlistId = get_option('tbm_floating_dm_playlist_id', '');
-        $this->adId = get_option('tbm_gam_ad_unit_id', '');
-        $this->playerTitle = "Editor's Picks";
+  public function __construct()
+  {
+    $this->playerId = 'x9m1x';
 
-        add_action('wp_footer', [$this, 'wp_footer']);
-    }
+    $this->playlistId = 'x6mqi7';
 
-    public function wp_footer() {
-        if (!is_single()) {
-            return;
-        }
-        if (!$this->playerId || '' == trim($this->playerId)) {
-            return;
-        }
-        if (!$this->playlistId || '' == trim($this->playlistId)) {
-            return;
-        }
-        if (function_exists('get_field') && (get_field('disable_ads', $post->id) || get_field('disable_ads_in_content'))) {
-            return;
-        }
+    $this->playlistId = get_option('tbm_floating_dm_playlist_id', 'x6mqi7');
+
+    $this->playerTitle = "Editor's Picks";
+
+    add_action('wp_footer', [$this, 'wp_footer']);
+  }
+
+  public function wp_footer()
+  {
+    if (!is_single())
+      return;
+
+    if (!$this->playlistId || '' == trim($this->playlistId))
+      return;
 ?>
-<style>
-    #floating-player-wrap {
+    <style>
+      #floating-player-wrap {
         right: 0;
         bottom: 65px;
         box-sizing: border-box;
@@ -60,15 +59,15 @@ class FloatingPlayer {
         max-width: 66%;
         flex-direction: column;
         padding: 0;
-    }
+      }
 
-    #floating-player-wrap.scrolled {
+      #floating-player-wrap.scrolled {
         top: 0;
         bottom: unset;
         flex-direction: column-reverse;
-    }
+      }
 
-    .floating-player-title {
+      .floating-player-title {
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
@@ -79,9 +78,9 @@ class FloatingPlayer {
         position: relative;
         font-family: Graphik, sans-serif;
         color: #fff;
-    }
+      }
 
-    .floating-player-close {
+      .floating-player-close {
         top: 0;
         background: none;
         color: #fff;
@@ -96,71 +95,74 @@ class FloatingPlayer {
         right: 0;
         text-align: center;
         z-index: 899;
-    }
+      }
 
-    @media(min-width: 48rem) {
+      @media(min-width: 48rem) {
         #floating-player-wrap {
-            right: 20px;
-            bottom: 20px;
-            display: block;
-            max-width: unset;
-            flex-direction: column;
+          right: 20px;
+          bottom: 20px;
+          display: block;
+          max-width: unset;
+          flex-direction: column;
         }
 
         #floating-player-wrap.scrolled {
-            top: unset;
-            bottom: 20px;
+          top: unset;
+          bottom: 20px;
         }
-    }
+      }
 
-    #dailymotion-pip-small-viewport {
+      #dailymotion-pip-small-viewport {
         --position-top: 0;
         --position-right: 0;
         max-width: 66%;
         left: unset !important;
         right: 0 !important;
-    }
-</style>
-
-<div id="floating-player-wrap" style="display: none">
-    <div class="floating-player-title">
+      }
+    </style>
+    <div id="floating-player-wrap" style="display: none">
+      <div class="floating-player-title">
         <span class="floating-player-close" style="display: inline;">x</span>
+      </div>
+      <div id="floating-player"></div>
     </div>
-    <div id="floating-player"></div>
-</div>
-<script src="https://geo.dailymotion.com/libs/player/<?php echo $this->playerId; ?>.js"></script>
-<script>
-    jQuery(document).ready(function($) {
+    <script src="https://geo.dailymotion.com/libs/player/<?php echo $this->playerId; ?>.js"></script>
+    <script>
+      jQuery(document).ready(function($) {
+
         $(window).on('scroll', function() {
-            if (screen.width < 768) {
-                if ($(window).scrollTop() >= screen.height / 2) {
-                    $('#floating-player-wrap').addClass('scrolled')
-                } else {
-                    $('#floating-player-wrap').removeClass('scrolled')
-                }
+          if (screen.width < 768) {
+            if ($(window).scrollTop() >= screen.height / 2) {
+              $('#floating-player-wrap').addClass('scrolled');
+
+            } else {
+              $('#floating-player-wrap').removeClass('scrolled');
             }
-        })
+          }
+        });
 
         $('.floating-player-close').on('click', function() {
-            $('#floating-player-wrap').detach()
+          $('#floating-player-wrap').detach();
         })
-        dailymotion.createPlayer("floating-player", {
-            playlist: '<?php echo $this->playlistId; ?>',
+        dailymotion
+          .createPlayer("floating-player", {
+            playlist: "<?php echo $this->playlistId; ?>",
             params: {
-                customConfig: {
-                    customParams: '/22071836792/<?php echo $this->adId; ?>/preroll'
-                }
+              customConfig: {
+                customParams: '/22071836792/SSM_thebragcomprem/preroll'
+              }
             }
-        })
-    .then((player) => {
-        $('#floating-player-wrap').show()
-        player.setMute(true)
-    })
-    .catch((e) => console.error(e))
-    })
-</script>
+          })
+          .then((player) => {
+            $('#floating-player-wrap').show();
+            player.setMute(true);
+          })
+          .catch((e) => console.error(e));
+      });
+    </script>
+
 <?php
-    } // wp_footer();
+  } // wp_footer();
 }
 
 new FloatingPlayer();
