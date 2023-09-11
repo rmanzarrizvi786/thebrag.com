985fa69b65c3cbd9340f2578ce306d6e53907c40
Toby Smith <tobypsmith@gmail.com>
2023-09-11 19:02:16 +1000

    fixing logo

--- plugins/theme-options/theme-options.php
+++ plugins/theme-options/theme-options.php
@@ -288,6 +288,10 @@ function tbm_create_spotlight_articles( $article_url ) {
         $height = '1rem';
     }
 
+    if( $filename == 'comingsoon' ) {
+        $height = '1.75rem';
+    }
+
     $articles_arr = [
         'image' => $article_remote_data['image'],
         'title' => $article_remote_data['title'],
